abffd1a1f672b6fc78564d7e23eda826b44b6465
qbressler <qbressler@gmail.com>
2015-09-01 17:20:26 -0400

    Initial Commit

---
+++ AspSpaSilo-GH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspSpaSilo_GH.MvcApplication" Language="C#" %>
---
+++ AspSpaSilo-GH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspSpaSilo_GH.Startup))]
+namespace AspSpaSilo_GH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
