c844f7ae803508a13928ed23fc6e916005104656
qbressler <qbressler@gmail.com>
2015-09-01 20:49:52 -0400

    9.1.2015

--- AspSpaSilo-GH/Properties/AssemblyInfo.cs
+++ AspSpaSilo-GH/Properties/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AspSpaSilo_GH")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mini Spa for ASP.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Quintin Bressler")]
 [assembly: AssemblyProduct("AspSpaSilo_GH")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
